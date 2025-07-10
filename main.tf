8aa096ccd9c38fe6d3ac011048750f17e22ba023
zacharykwek15 <zhi_hong_sn@yahoo.com.sg>
2025-07-10 20:23:59 +0800

    adding new s3 bucket

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce10-tfstate"
+    key    = "terraform.tfstate"  #Change this
+    region = "ap-southeast-1"
+  }
+}
+
+resource "aws_s3_bucket" "s3_tf" {
+  bucket_prefix =  "zh-bucket"   # Set your bucket name here
+}
