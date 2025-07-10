989498542dd83fd2fba5ad0b2bfbd6369fe8615d
zacharykwek15 <zhi_hong_sn@yahoo.com.sg>
2025-07-10 21:26:59 +0800

    .

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sctp-ce10-tfstate"
-    key    = "terraform.tfstate"  #Change this
+    key    = "zh-terraform.tfstate" #Change this
     region = "ap-southeast-1"
   }
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket_prefix =  "zh-bucket-3"   # Set your bucket name here
+  bucket_prefix = "zh-bucket-3" # Set your bucket name here
 }
