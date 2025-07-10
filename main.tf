e9c56f5886c12b745556855fe7108924fb5e4c27
zacharykwek15 <zhi_hong_sn@yahoo.com.sg>
2025-07-10 21:54:00 +0800

    .

--- main.tf
+++ main.tf
@@ -1,30 +1,25 @@
-provider "aws" {
-  region = "ap-southeast-1"
-}
-
 terraform {
   required_version = ">= 1.9.0"
 
   backend "s3" {
     bucket = "sctp-ce10-tfstate"
-    key    = "zh-terraform.tfstate" #Change this
+    key    = "zh-terraform.tfstate" # Change this
     region = "ap-southeast-1"
   }
 }
 
-resource "aws_s3_bucket" "s3_tf" {
-  #checkov:skip=CKV2_AWS_6: "Ensure that S3 bucket has a Public Access block"
-  #checkov:skip=CKV_AWS_145: "Ensure that S3 buckets are encrypted with KMS by default"
-  #checkov:skip=CKV2_AWS_62: "Ensure S3 buckets should have event notifications enabled"
-  #checkov:skip=CKV_AWS_18: "Ensure the S3 bucket has access logging enabled"
-  #checkov:skip=CKV_AWS_144: "Ensure that S3 bucket has cross-region replication enabled"
-  #checkov:skip=CKV_AWS_21: "Ensure all data stored in the S3 bucket have versioning enabled"
-  #checkov:skip=CKV2_AWS_61: "Ensure that an S3 bucket has a lifecycle configuration"
-  bucket_prefix = "zh-bucket-3" # Set your bucket name here
-
+provider "aws" {
+  region = "ap-southeast-1"
 }
 
-#resource "aws_s3_bucket" "s3_tf" {
+resource "aws_s3_bucket" "s3_tf" {
+  # checkov:skip=CKV2_AWS_6: "Ensure that S3 bucket has a Public Access block"
+  # checkov:skip=CKV_AWS_145: "Ensure that S3 buckets are encrypted with KMS by default"
+  # checkov:skip=CKV2_AWS_62: "Ensure S3 buckets should have event notifications enabled"
+  # checkov:skip=CKV_AWS_18: "Ensure the S3 bucket has access logging enabled"
+  # checkov:skip=CKV_AWS_144: "Ensure that S3 bucket has cross-region replication enabled"
+  # checkov:skip=CKV_AWS_21: "Ensure all data stored in the S3 bucket have versioning enabled"
+  # checkov:skip=CKV2_AWS_61: "Ensure that an S3 bucket has a lifecycle configuration"
 
-  #bucket_prefix = lower("${local.name_prefix}-sctp-bkt")
-#}
+  bucket_prefix = "zh-bucket-3"
+}
