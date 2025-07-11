79f997a3d4f98b40819732c7327ef8cde7321f2e
zacharykwek15 <zhi_hong_sn@yahoo.com.sg>
2025-07-11 19:15:49 +0800

    .

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.9.0"
 
+  required_providers { 
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+
   backend "s3" {
     bucket = "sctp-ce10-tfstate"
     key    = "zh-terraform.tfstate" # Change this
@@ -10,6 +17,7 @@ terraform {
 
 provider "aws" {
   region = "ap-southeast-1"
+
 }
 
 resource "aws_s3_bucket" "s3_tf" {
